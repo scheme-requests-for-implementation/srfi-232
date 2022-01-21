2461a88020b3c58b55c88e8288f1bc9f3c3708bb
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-01-21 13:36:24 -0500

    Update tests.

--- test-body.scm
+++ test-body.scm
@@ -7,6 +7,7 @@
   (test-eqv 5 (((lambda* (w x y z) (+ w x y z)) 1 1) 1 2))
   (test-eqv 5 (((lambda* (w x y z) (+ w x y z)) 1 1 1) 2))
   (test-eqv 5 (((((lambda* (w x y z) (+ w x y z)) 1) 1) 1) 2))
+  (test-eqv 4 (((((((((lambda* (a b c) 4)))))))) 1 2 3))
   )
 
 (test-group "Variadic"
@@ -16,20 +17,6 @@
    '(3 (3 4))
    (((lambda* (a b . rest) (list (+ a b) rest)) 1) 2 3 4))
   (test-equal '(3 ()) ((lambda* (a b . rest) (list (+ a b) rest)) 1 2))
-  )
-
-(test-group "Nullary"
-  (test-eqv 3 ((lambda* () (lambda* (x y) (+ x y))) 1 2))
-  (test-eqv 3 (((lambda* () (lambda* (x y) (+ x y))) 1) 2))
-
-  ;; "... while these behaviors are decidedly not wrong, they are
-  ;;  perhaps mildly unsettling."
-  (test-eqv 2
-            ((lambda* (a)
-               (lambda* ()
-                 (lambda* ()
-                   (lambda* (b) b)))) 1 2))
-  (test-eqv 4 (((((((((lambda* (a b c) 4)))))))) 1 2 3))
   )
 
 (test-group "Extra arguments"
